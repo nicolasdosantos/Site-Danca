ae2e455f686bcecc093d8e8dacb0c2a7a2cbf33c
nicolasdosantos <104533715+nicolasdosantos@users.noreply.github.com>
2023-11-24 10:54:14 -0300

    ------------

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 var ver = document.getElementById('ver-mais');
 
-ver.addEventListener("click", function () {
+ver.addEventListener("click", function () { 
     var estilos = document.getElementById("estilos-div2")
+    var animacao = document.getElementsByClassName("card-danca-baixo")
 
     if (estilos.style.display === "flex") {
-        estilos.style.display = "none";
         ver.innerHTML = `<span class="button_top">
                             VER MAIS!
                         </span>`
+        for (var i = 0; i < animacao.length; i++) {
+            animacao[i].style.animationName = "entrada-baixo"
+        }
+        setTimeout(() => {
+            estilos.style.display = "none";
+        }, 380)
+        
     }
     else {
         estilos.style.display = "flex";
         ver.innerHTML = `<span class="button_top">
                             VER MENOS
                         </span>`
+        for (var i = 0; i < animacao.length; i++) {
+            animacao[i].style.animationName = "entrada-cima"
+        }
     }
 });
